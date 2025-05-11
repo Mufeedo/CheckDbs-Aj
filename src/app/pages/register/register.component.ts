6bfd0892efa88ec47a742a4c82a2fa03f474631a
Mufeedo <mufeedom@gmail.com>
2025-05-11 11:02:02 +0530

    fic: reg. userorganization

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  selectedIndex: number = 0;
+
+  constructor(private router: Router) {}
+
+  setTab(index: number) {
+    // Set the active tab when the user clicks a tab manually
+    this.selectedIndex = index;
+
+  }
+  onSubmit(event: Event) {
+    event.preventDefault(); // Optional: Just in case
+    this.nextTab(); 
+    
+    // Your actual submit logic
+  }
+
+  nextTab() {
+    // Move to the next tab, ensuring it does not exceed the number of tabs
+    if (this.selectedIndex < 2) {
+      this.selectedIndex++;
+    }
+  }
+
+  submitForm() {
+    // this.router.navigate(['/']);
+  }
 
 }
